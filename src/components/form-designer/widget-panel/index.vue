cf79cddc7301526a73688dcc196fd0ed726e3181
xuekaikai <2780739366@qq.com>
2022-09-17 21:42:00 +0800

    左侧字段demo

--- src/components/form-designer/widget-panel/index.vue
+++ src/components/form-designer/widget-panel/index.vue
@@ -1,11 +1,39 @@
 <template>
     <el-scrollbar class="side-scroll-bar">
         <el-collapse class="widget-collapse" value="activeName">
-            <el-collapse-item name="容器" title="容器"></el-collapse-item>
-            <el-collapse-item
-                name="基础字段"
-                title="基础字段"
-            ></el-collapse-item>
+            <el-collapse-item name="容器" title="容器"
+                >w1sdf11sdq1</el-collapse-item
+            >
+            <el-collapse-item name="基础字段" title="基础字段"
+                ><Draggable
+                    tag="ul"
+                    :list="basicFields"
+                    :group="{ name: 'dragGroup', pull: 'clone', put: false }"
+                    :clone="handleBasicFieldWidgetClone"
+                    ghost-class="ghost"
+                    :sort="false"
+                    itemKey="id"
+                    style="padding: 3px"
+                >
+                    <template #item="{ element }">
+                        <div
+                            class="container-widget-item"
+                            :title="element.title"
+                        >
+                            <span>
+                                <el-icon
+                                    style="
+                                        vertical-align: middle;
+                                        margin-right: 3px;
+                                    "
+                                    ><Loading
+                                /></el-icon>
+                                {{ element.title }}
+                            </span>
+                        </div>
+                    </template>
+                </Draggable>
+            </el-collapse-item>
             <el-collapse-item
                 name="高级字段"
                 title="高级字段"
@@ -19,12 +47,27 @@
 </template>
 
 <script>
+import Draggable from 'vuedraggable'
+import { basicFields } from './widgetsConfig'
 export default {
     name: 'widget-panel',
+    components: {
+        Draggable,
+    },
     data() {
         return {
             activeName: ['容器', '基础字段', '高级字段', '自定义字段'],
+            basicFields: [],
         }
+    },
+    mounted() {
+        this.loadWidgets()
+    },
+    methods: {
+        loadWidgets() {
+            this.basicFields = basicFields
+        },
+        handleBasicFieldWidgetClone() {},
     },
 }
 </script>
@@ -37,5 +80,28 @@ export default {
 }
 
 .widget-collapse {
+    border-top-width: 0px;
+
+    ::v-deep .el-collapse-item__header {
+        margin-left: 8px;
+        font-style: italic;
+        font-weight: bold;
+    }
+
+    .container-widget-item,
+    .field-widget-item {
+        display: inline-block;
+        height: 28px;
+        line-height: 28px;
+        width: 115px;
+        // float: left;
+        padding-left: 6px;
+        margin: 0px 6px 0px 0px;
+        cursor: move;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        background-color: #f1f2f3;
+    }
 }
 </style>
---
+++ src/components/form-designer/widget-panel/widgetsConfig.js
@@ -0,0 +1,21 @@
+// widget 配置
+export const container = {
+
+}
+
+export const basicFields = [
+    {
+        id: 0,
+        type: 'input',
+        title: '单行输入',
+        icon: 'el-icon-phone-outline',
+        formItemFlag: true
+    },
+    {
+        id: 1,
+        type: 'button',
+        title: '按钮',
+        icon: 'el-icon-phone-outline',
+        formItemFlag: false
+    }
+]
